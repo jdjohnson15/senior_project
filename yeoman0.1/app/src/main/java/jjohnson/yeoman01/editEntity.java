49e67bbb3058d0549419eb11cb280f512e668f9c
Carolyn Owens <ciowens15@transy.edu>
2015-03-24 00:58:42 -0400

    Edit Entity 0.1
    Not working at all. Just pushing this out there.

--- yeoman0.1/app/src/main/java/jjohnson/yeoman01/editEntity.java
+++ yeoman0.1/app/src/main/java/jjohnson/yeoman01/editEntity.java
@@ -1,6 +1,8 @@
 package jjohnson.yeoman01;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -17,6 +19,36 @@ public class editEntity extends ActionBarActivity {
         setContentView(R.layout.activity_edit_entity);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        // Get the name of Entity from user
+        // For now, Kasgar is a safe value
+        String entityName = "Kasgar";
+        int[] entityStats = new int[6];
+
+        SQLiteDatabase yeomanDB = openOrCreateDatabase("Yeoman",MODE_PRIVATE,null);
+        Cursor resultSet = yeomanDB.rawQuery("Select * from Character WHERE Name = ?", new String[] {entityName});
+
+        resultSet.moveToFirst();
+        entityStats[0] = resultSet.getInt(0);
+        System.out.println("Str: " + entityStats[0]);
+        int i = 0;
+        while (resultSet.moveToNext())
+        {
+            entityStats[i] = resultSet.getInt(0);
+            System.out.println("Stat: " + entityStats[i]);
+            i++;
+        }
+        // entityStats now contains the entity's stats.
+        // In order:
+        // 0 = str, 1 = dex, 2 = con, 3 = int, 4 = wis, 5 = cha
+        // Just stick it into whatever to display it.
+
+        // Get values from user, put them directly into entityStats (and entityName, if need be)
+
+
+
+
+
     }
 
 
